056ed173c13cfb255dd25c9096435ab6cae47d39
Christian Burkhart <worm@christianburkhart.com>
2018-10-26 00:11:31 -0700

    added for check for null abv field

--- scripts/cocktail_abv.js
+++ scripts/cocktail_abv.js
@@ -5,26 +5,36 @@ function cocktail_abv(ingredients_volume) {
 
   for (i = 0; i < ingredients.length; i++) {
 
-    var unit = ingredients[i].attr("Unit");
-    var abv = ingredients[i].field("ABV");
-    var amount = ingredients[i].attr("Amount");
+    if (ingredients[i].field("ABV") !== null); {
 
-    switch (unit) {
-      case "oz":
-        sum += amount*abv;
-        break;
-      case "Dash":
-        sum += (amount*0.0311)*abv;
-        break;
-      case "Barspoon":
-        sum += (amount*0.167)*abv;
-        break;
-      case "Piece":
-        sum += 0;
-        break;
-      case "Drop":
-        sum += (amount*0.0016907)*abv;
-        break;
+      var unit = ingredients[i].attr("Unit");
+      var abv = ingredients[i].field("ABV");
+      var amount = ingredients[i].attr("Amount");
+
+      switch (unit) {
+        case "oz":
+          sum += amount*abv;
+          break;
+        case "Dash":
+          sum += (amount*0.0311)*abv;
+          break;
+        case "Barspoon":
+          sum += (amount*0.167)*abv;
+          break;
+        case "Piece":
+          sum += 0;
+          break;
+        case "Drop":
+          sum += (amount*0.0016907)*abv;
+          break;
+
+      }
+    }
+
+    else {
+
+      sum += 0;
+
     }
   }
 
