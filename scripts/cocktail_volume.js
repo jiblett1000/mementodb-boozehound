d4c1e8918ac22a684548485dda7727b51ae7bf35
Christian Burkhart <worm@christianburkhart.com>
2018-10-31 04:52:01 -0700

    convert if else statements with switch

--- scripts/cocktail_volume.js
+++ scripts/cocktail_volume.js
@@ -3,7 +3,6 @@
 function initial_volume(){
 
 var ingredients = field("Ingredients");
-var i;
 var sum = 0;
 
 for (i = 0; i < ingredients.length; i++) {
@@ -11,22 +10,24 @@ for (i = 0; i < ingredients.length; i++) {
   var unit = ingredients[i].attr("Unit");
   var amount = ingredients[i].attr("Amount");
 
-  if (unit == "oz") {
-     sum += amount;
-  }
-  else if (unit == "Dash") {
-    sum += amount*0.0311;
-  }
-  else if (unit == "Barspoon") {
-    sum += amount*0.167;
-  }
-  else if (unit == "Piece") {
-    sum += 0;
-  }
-  else if (unit == "Drop") {
-    sum += amount*0.0016907;
-  }
+  switch (unit) {
+    case "oz":
+      sum += amount;
+      break;
+    case "Dash":
+      sum += amount*0.0311;
+      break;
+    case "Barspoon":
+      sum += amount*0.167;
+      break;
+    case "Piece":
+      sum += 0;
+      break;
+    case "Drop":
+      sum += amount*0.0016907;
+    default:
 
+  }
 
 }
 
