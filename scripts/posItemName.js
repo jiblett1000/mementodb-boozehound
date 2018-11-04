48bfb52f7b5b8a36de0391ac3dc5e539cee649ca
Christian Burkhart <worm@christianburkhart.com>
2018-11-03 22:48:17 -0700

    changed styling

--- scripts/posItemName.js
+++ scripts/posItemName.js
@@ -1,17 +1,13 @@
 function posItemName() {
-  var manualName = field("Manual Name");
-  var inventory = field("White Cap Inventory")[0];
-  var cocktail = field("White Cap Cocktail Specs")[0];
+  const manualName = field('Manual Name');
+  const inventory = field('White Cap Inventory')[0];
+  const cocktail = field('White Cap Cocktail Specs')[0];
 
   if (manualName) {
     manualName;
-  }
-
-  else if (inventory) {
-    inventory.field("Thing")[0].field("Name");
-  }
-
-  else if (cocktail) {
-    cocktail.field("Name");
+  } else if (inventory) {
+    inventory.field('Thing')[0].field('Name');
+  } else if (cocktail) {
+    cocktail.field('Name');
   }
 }
