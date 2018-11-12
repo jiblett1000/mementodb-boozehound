59fbf1edee9aaf199b97248ba509a603c4b4d513
Christian Burkhart <worm@christianburkhart.com>
2018-11-11 21:03:56 -0800

    fix?

--- scripts/cocktailDilution.js
+++ scripts/cocktailDilution.js
@@ -2,10 +2,10 @@ function cocktailDilution() {
   const initialAbv = field('Initial ABV') / 100;
   const prepMethods = field('Prep Method(s)');
   const formula = {
-    'Stir': -1.21 * (initialAbv ** 2) + 1.246 * initialAbv + 0.145,
-    'Shake': -1.567 * (initialAbv ** 2) + 1.742 * initialAbv + 0.203,
-    'Short Shake': this.Shake * 0.75,
-  }
+    'Stir': function stir() { return -1.21 * (initialAbv ** 2) + 1.246 * initialAbv + 0.145; },
+    'Shake': function shake() { return -1.567 * (initialAbv ** 2) + 1.742 * initialAbv + 0.203; },
+    'Short Shake': function shortShake() { return this.shake() * 0.75; },
+  };
 
   const prepTotal = prepMethods.reduce((sum, meth) => sum + formula[meth.field('Name')], 0);
 
