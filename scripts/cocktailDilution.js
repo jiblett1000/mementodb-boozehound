1630d3bdd2e27d1a64f82c78a5bec532d11d0ae8
Christian Burkhart <worm@christianburkhart.com>
2018-11-11 21:40:10 -0800

    over it

--- scripts/cocktailDilution.js
+++ scripts/cocktailDilution.js
@@ -1,20 +1,25 @@
 function cocktailDilution() {
   const initialAbv = field('Initial ABV') / 100;
   const prepMethods = field('Prep Method(s)');
-  const formula = {
-    'Stir': () => -1.21 * (initialAbv ** 2) + 1.246 * initialAbv + 0.145,
-    'Stir on Big Rock': this.Stir,
-    'Shake': () => -1.567 * (initialAbv ** 2) + 1.742 * initialAbv + 0.203,
-    'Short Shake': () => this.Shake * 0.75,
+  const formulas = {
+    stir() { return -1.21 * (initialAbv ** 2) + 1.246 * initialAbv + 0.145; },
+    shake() { return -1.567 * (initialAbv ** 2) + 1.742 * initialAbv + 0.203; },
+    shortShake() { return this.shake() * 0.75; },
+  };
+  const dilution = {
+    'Stir': formulas.stir(),
+    'Stir on Big Rock': formulas.stir(),
+    'Shake': formulas.shake(),
+    'Short Shake': formulas.shake() * 0.75,
+    'Dirty Dump': formulas.shake(),
     'Build': 0,
-    'Dirty Dump': this.Shake,
     'Dry Shake': 0,
     'Slushy': 0,
     'Muddle': 0,
     'Fine Strain': 0,
     'Swizzle': 0,
   };
-  const prepTotal = prepMethods.reduce((sum, meth) => sum + formula[meth.field('Name')], 0);
+  const prepTotal = prepMethods.reduce((sum, meth) => sum + dilution[meth.field('Name')], 0);
 
   return prepTotal;
 }
