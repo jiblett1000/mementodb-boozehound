6a1e7bc22cc7f75195d8e640347e315955c28ac5
Christian Burkhart <worm@christianburkhart.com>
2018-11-11 18:05:21 -0800

    test for short shake

--- scripts/cocktailDilution.js
+++ scripts/cocktailDilution.js
@@ -4,7 +4,7 @@ function cocktailDilution() {
   const formula {
     'Stir': -1.21 * (initialAbv ** 2) + 1.246 * initialAbv + 0.145,
     'Shake': -1.567 * (initialAbv ** 2) + 1.742 * initialAbv + 0.203,
-    'Short Shake': (-1.567 * (initialAbv ** 2) + 1.742 * initialAbv + 0.203) * 0.75,
+    'Short Shake': (Shake) => Shake * 0.75,
   }
 
   const prepTotal = prepMethods.reduce((sum, meth) => {
