bdabe9233ceb8a517095512843a3c73c8a8197fb
Christian Burkhart <worm@christianburkhart.com>
2018-11-11 21:06:38 -0800

    hope

--- scripts/cocktailDilution.js
+++ scripts/cocktailDilution.js
@@ -4,9 +4,8 @@ function cocktailDilution() {
   const formula = {
     'Stir': function stir() { return -1.21 * (initialAbv ** 2) + 1.246 * initialAbv + 0.145; },
     'Shake': function shake() { return -1.567 * (initialAbv ** 2) + 1.742 * initialAbv + 0.203; },
-    'Short Shake': function shortShake() { return this.shake() * 0.75; },
+    'Short Shake': function shortShake() { return this['Shake'] * 0.75; },
   };
-
   const prepTotal = prepMethods.reduce((sum, meth) => sum + formula[meth.field('Name')], 0);
 
   return prepTotal;
