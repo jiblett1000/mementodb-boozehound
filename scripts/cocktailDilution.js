ca3c043c24dd9337031e5a2ffb684da023869e66
Christian Burkhart <worm@christianburkhart.com>
2018-11-11 21:44:38 -0800

    fixes

--- scripts/cocktailDilution.js
+++ scripts/cocktailDilution.js
@@ -7,17 +7,17 @@ function cocktailDilution() {
     shortShake() { return this.shake() * 0.75; },
   };
   const dilution = {
-    'Stir': formulas.stir(),
+    Stir: formulas.stir(),
     'Stir on Big Rock': formulas.stir(),
-    'Shake': formulas.shake(),
-    'Short Shake': formulas.shake() * 0.75,
+    Shake: formulas.shake(),
+    'Short Shake': formulas.shortShake(),
     'Dirty Dump': formulas.shake(),
-    'Build': 0,
+    Build: 0,
     'Dry Shake': 0,
-    'Slushy': 0,
-    'Muddle': 0,
+    Slushy: 0,
+    Muddle: 0,
     'Fine Strain': 0,
-    'Swizzle': 0,
+    Swizzle: 0,
   };
   const prepTotal = prepMethods.reduce((sum, meth) => sum + dilution[meth.field('Name')], 0);
 
