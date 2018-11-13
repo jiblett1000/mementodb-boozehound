80d562253ba9af75add880a9017bf9499720998e
Christian Burkhart <worm@christianburkhart.com>
2018-11-13 04:10:03 -0800

    test

--- scripts/cocktailGlassFilled.js
+++ scripts/cocktailGlassFilled.js
@@ -1,12 +1,11 @@
 function cocktailGlassFilled() {
-  const e = entry();
-  const drinkware = e.field('Drinkware');
+  const drinkware = field('Drinkware');
 
   if (drinkware.length === 0) {
-    e.set('% of Glass Filled', 'Glassware not selected.');
-  } else {
-    const served = e.field('Served')[0].field('Name');
-    const finalVolume = e.field('Final Volume (oz)');
+    return 'Glassware not selected.';
+  }
+    const served = field('Served')[0].field('Name');
+    const finalVolume = field('Final Volume (oz)');
     const drinkwareCapacity = drinkware[0].field('Capacity (oz)');
     const iceVolumes = {
       'On the Rocks': 0.0811,
@@ -21,6 +20,6 @@ function cocktailGlassFilled() {
     const iceAmount = Math.floor(drinkwareCapacity / iceVolume);
     const percent = ((finalVolume + (iceAmount * iceVolume)) / drinkwareCapacity).toFixed(2);
 
-    e.set('% of Glass Filled', percent);
-  }
+    return percent;
+
 }
