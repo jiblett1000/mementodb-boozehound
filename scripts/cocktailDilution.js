9582049d9d20a0099862326d1118a62e4407d1c0
Christian Burkhart <worm@christianburkhart.com>
2018-11-12 20:51:09 -0800

    fixed exponent operator error.

--- scripts/cocktailDilution.js
+++ scripts/cocktailDilution.js
@@ -1,17 +1,17 @@
 function cocktailDilution(initialAbvPercent, prepMethods) {
   const initialAbv = initialAbvPercent / 100;
   const dilute = {
-    stir(abv) { return -1.21 * (abv ** 2) + 1.246 * abv + 0.145; },
-    shake(abv) { return -1.567 * (abv ** 2) + 1.742 * abv + 0.203; },
+    stir() { return -1.21 * Math.pow(initialAbv, 2) + 1.246 * initialAbv + 0.145; },
+    shake() { return -1.567 * Math.pow(initialAbv, 2) + 1.742 * initialAbv + 0.203; },
     shortShake() { return this.shake() * 0.75; },
   };
 
   const dilution = {
-    Stir: dilute.stir(initialAbv),
-    'Stir on Big Rock': dilute.stir(initialAbv),
-    Shake: dilute.shake(initialAbv),
-    'Short Shake': dilute.shortShake(initialAbv),
-    'Dirty Dump': dilute.shake(initialAbv),
+    Stir: dilute.stir(),
+    'Stir on Big Rock': dilute.stir(),
+    Shake: dilute.shake(),
+    'Short Shake': dilute.shortShake(),
+    'Dirty Dump': dilute.shake(),
     Build: 0,
     'Dry Shake': 0,
     Slushy: 0,
