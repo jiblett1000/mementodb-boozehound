15684628ac7ac074923da862b8acd1ba4f4fe7b7
Christian Burkhart <worm@christianburkhart.com>
2018-11-13 04:03:32 -0800

    test

--- scripts/cocktailGlassFilled.js
+++ scripts/cocktailGlassFilled.js
@@ -3,23 +3,24 @@ function cocktailGlassFilled() {
   const drinkware = e.field('Drinkware');
 
   if (drinkware.length === 0) {
-    return 'Glassware not selected.';
-  }
-  const served = e.field('Served')[0].field('Name');
-  const finalVolume = e.field('Final Volume (oz)');
-  const drinkwareCapacity = drinkware[0].field('Capacity (oz)');
-  const iceVolumes = {
-    'On the Rocks': 0.0811,
-    'On Collins Cube': 4,
-    'On Crushed': drinkwareCapacity - finalVolume,
-    'On a Big Rock': 4.432899285,
-    'Neat': 0,
-    'Slushy': 0,
-    'Up': 0,
-  };
-  const iceVolume = iceVolumes[served];
-  const iceAmount = Math.floor(drinkwareCapacity / iceVolume);
-  const percent = ((finalVolume + (iceAmount * iceVolume)) / drinkwareCapacity).toFixed(2);
+    e.set('% of Glass Filled', 'Glassware not selected.');
+  } else {
+    const served = e.field('Served')[0].field('Name');
+    const finalVolume = e.field('Final Volume (oz)');
+    const drinkwareCapacity = drinkware[0].field('Capacity (oz)');
+    const iceVolumes = {
+      'On the Rocks': 0.0811,
+      'On Collins Cube': 4,
+      'On Crushed': drinkwareCapacity - finalVolume,
+      'On a Big Rock': 4.432899285,
+      'Neat': 0,
+      'Slushy': 0,
+      'Up': 0,
+    };
+    const iceVolume = iceVolumes[served];
+    const iceAmount = Math.floor(drinkwareCapacity / iceVolume);
+    const percent = ((finalVolume + (iceAmount * iceVolume)) / drinkwareCapacity).toFixed(2);
 
-  e.set('% of Glass Filled', percent);
+    e.set('% of Glass Filled', percent);
+  }
 }
