038b73f2c5520b5cfbdaab0a3f9cfe961c6ab916
Christian Burkhart <worm@christianburkhart.com>
2018-11-12 23:49:12 -0800

    refactoring

--- scripts/cocktailDilution.js
+++ scripts/cocktailDilution.js
@@ -1,27 +1,21 @@
 function cocktailDilution(initialAbvPercent, prepMethods) {
   const initialAbv = initialAbvPercent / 100;
-  /*
-  const dilute = {
-    stir() { return -1.21 * Math.pow(initialAbv, 2) + 1.246 * initialAbv + 0.145; },
-    shake() { return -1.567 * Math.pow(initialAbv, 2) + 1.742 * initialAbv + 0.203; },
-    shortShake() { return this.shake() * 0.75; },
-  };
-*/
+
   const dilution = {
-    Stir: () => -1.21 * Math.pow(initialAbv, 2) + 1.246 * initialAbv + 0.145,
+    'Stir': () => -1.21 * Math.pow(initialAbv, 2) + 1.246 * initialAbv + 0.145,
     'Stir on Big Rock': () => this['Stir'](),
-    Shake: () => -1.567 * Math.pow(initialAbv, 2) + 1.742 * initialAbv + 0.203,
+    'Shake': () => -1.567 * Math.pow(initialAbv, 2) + 1.742 * initialAbv + 0.203,
     'Short Shake': () => this['Shake']() * 0.75,
     'Dirty Dump': () => this['Shake'](),
-    Build: 0,
+    'Build': 0,
     'Dry Shake': 0,
-    Slushy: 0,
-    Muddle: 0,
+    'Slushy': 0,
+    'Muddle': 0,
     'Fine Strain': 0,
-    Swizzle: 0,
+    'Swizzle': 0,
   };
-  entry().set('Dilution %', (prepMethods.reduce((sum, meth) => {
-    return sum + dilution[meth.field('Name')], 0)).toFixed(2);
-});
 
+  const dilutionPercent = prepMethods.reduce((sum, meth) => sum + dilution[meth.field('Name')], 0);
+
+  entry().set('Dilution %', dilutionPercent.toFixed(2));
 }
