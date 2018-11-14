6f8ea31b3c66544c83a29e6102a701798ead5844
Christian Burkhart <worm@christianburkhart.com>
2018-11-14 00:41:41 -0800

    completed settings map

--- scripts/cocktail.js
+++ scripts/cocktail.js
@@ -1,14 +1,15 @@
-const userSettings = {
-  businessName: this.businessName,
-  volumeUnits: libByName(this.businessName + " " + 'Settings').entry().field(''),
-}
+// User Settings:
+
+const businessName = libByName(this.businessName + ' ' + 'Settings').findByKey('Business Name').field('Value');
+const volumeUnits = libByName(this.businessName + ' ' + 'Settings').findByKey('Volume Units').field('Value');
+const massUnits = libByName(this.businessName + ' ' + 'Settings').findByKey('Mass Units').field('Value');
+const salesTaxRate = libByName(this.businessName + ' ' + 'Settings').findByKey('Sales Tax Rate').field('Value');
+const decimalPlaceAccuracy = libByName(this.businessName + ' ' + 'Settings').findByKey('Decimal Place Accuracy').field('Value');
+
 
 const cocktail = {
   name: entry().field('Name'),
   ingredients: entry().field('Ingredients'),
-  ingredient: {
-    amount: ,
-  },
   initialVolume: entry().field('Initial Volume (oz)'),
   initialAbv: entry().field('Initial ABV'),
   dilutionPercent: entry().field('Dilution %'),
