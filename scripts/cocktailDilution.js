f2d208cd99ea33d64438c59e425125bed41ceb17
Christian Burkhart <worm@christianburkhart.com>
2018-11-13 19:39:58 -0800

    test fix

--- scripts/cocktailDilution.js
+++ scripts/cocktailDilution.js
@@ -1,21 +1,22 @@
 function cocktailDilution(initialAbvPercent, prepMethods) {
   const initialAbv = initialAbvPercent / 100;
-
+  function stir() { -1.21 * Math.pow(initialAbv, 2) + 1.246 * initialAbv + 0.145; }
+  function shake() { -1.567 * Math.pow(initialAbv, 2) + 1.742 * initialAbv + 0.203; }
   const dilution = {
-    'Stir': () => -1.21 * Math.pow(initialAbv, 2) + 1.246 * initialAbv + 0.145,
-    'Stir on Big Rock': () => -1.21 * Math.pow(initialAbv, 2) + 1.246 * initialAbv + 0.145,
-    'Shake': () => -1.567 * Math.pow(initialAbv, 2) + 1.742 * initialAbv + 0.203,
-    'Short Shake': () => (-1.567 * Math.pow(initialAbv, 2) + 1.742 * initialAbv + 0.203) * 0.75,
-    'Dirty Dump': () => -1.567 * Math.pow(initialAbv, 2) + 1.742 * initialAbv + 0.203,
-    'Build': () => 0,
-    'Dry Shake': () => 0,
-    'Slushy': () => 0,
-    'Muddle': () => 0,
-    'Fine Strain': () => 0,
-    'Swizzle': () => 0,
+    'Stir': stir() ,
+    'Stir on Big Rock': stir(),
+    'Shake': shake(),
+    'Short Shake': shake() * 0.75,
+    'Dirty Dump': shake(),
+    'Build': 0,
+    'Dry Shake': 0,
+    'Slushy': 0,
+    'Muddle': 0,
+    'Fine Strain': 0,
+    'Swizzle': 0,
   };
 
-  const dilutionPercent = prepMethods.reduce((sum, meth) => sum + dilution[meth.field('Name')](), 0);
+  const dilutionPercent = prepMethods.reduce((sum, meth) => sum + dilution[meth.field('Name')], 0);
 
   // Change dilution into '50%' as opposed to '0.5' and then round. Set field.
 
