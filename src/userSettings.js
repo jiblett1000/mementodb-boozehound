dd4f35df23347550128cc081a9b76f73bedbcee1
Christian Burkhart <worm@christianburkhart.com>
2018-11-14 22:29:36 -0800

    test

--- src/userSettings.js
+++ src/userSettings.js
@@ -1,5 +1,5 @@
-const businessName = libByName('Boozehound Settings').findByKey('Business Name').field('Value');
-const volumeUnits = libByName('Boozehound Settings').findByKey('Volume Units').field('Value');
-const massUnits = libByName('Boozehound Settings').findByKey('Mass Units').field('Value');
-const salesTaxRate = libByName('Boozehound Settings').findByKey('Sales Tax Rate').field('Value');
-const decimalPlaceAccuracy = libByName('Boozehound Settings').findByKey('Decimal Place Accuracy').field('Value');
+export const businessName = libByName('Boozehound Settings').findByKey('Business Name').field('Value');
+export const volumeUnits = libByName('Boozehound Settings').findByKey('Volume Units').field('Value');
+export const massUnits = libByName('Boozehound Settings').findByKey('Mass Units').field('Value');
+export const salesTaxRate = libByName('Boozehound Settings').findByKey('Sales Tax Rate').field('Value');
+export const decimalPlaceAccuracy = libByName('Boozehound Settings').findByKey('Decimal Place Accuracy').field('Value');
