3a8ef23316475812fe5ac73f451a271f8393c2bf
Christian Burkhart <worm@christianburkhart.com>
2018-11-14 19:33:33 -0800

    testing

--- src/userSettings.js
+++ src/userSettings.js
@@ -1,4 +1,4 @@
-export const businessName = libByName(this.businessName + ' ' + 'Settings').findByKey('Business Name').field('Value');
+export const businessName = libByName('Boozehound' + ' ' + 'Settings').findByKey('Business Name').field('Value');
 export const volumeUnits = libByName(this.businessName + ' ' + 'Settings').findByKey('Volume Units').field('Value');
 export const massUnits = libByName(this.businessName + ' ' + 'Settings').findByKey('Mass Units').field('Value');
 export const salesTaxRate = libByName(this.businessName + ' ' + 'Settings').findByKey('Sales Tax Rate').field('Value');
