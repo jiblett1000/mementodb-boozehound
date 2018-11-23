6041e7234d1777481ab6935390ad9080989a154d
Christian Burkhart <worm@christianburkhart.com>
2018-11-23 00:40:46 -0800

    test

--- dist/usrPref.js
+++ dist/usrPref.js
@@ -4,14 +4,22 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.volUnits = exports.bizName = void 0;
-var bizName = libByName('Boozehound Settings').findByKey('Business Name').field('Value');
+
+var bizName = function bizName() {
+  return libByName('Boozehound Settings').findByKey('Business Name').field('Value');
+};
+
 exports.bizName = bizName;
-var volUnits = libByName("".concat(bizName, " Settings")).findByKey('Volume Units').field('Value');
+
+var volUnits = function volUnits() {
+  return libByName("".concat(bizName(), " Settings")).findByKey('Volume Units').field('Value');
+};
 /*  massUnits: () => libByName(`${this.bizName} Settings`).findByKey('Mass Units').field('Value'),
   salesTaxRate: () => libByName(`${this.bizName} Settings`).findByKey('Sales Tax Rate').field('Value'),
   decPlaceAcc: () => libByName(`${this.bizName} Settings`).findByKey('Decimal Place Accuracy').field('Value'),
 
 */
+
 
 exports.volUnits = volUnits;
 //# sourceMappingURL=../usrPref.js.map
--- src/usrPref.js
+++ src/usrPref.js
@@ -1,6 +1,6 @@
 
-const bizName = libByName('Boozehound Settings').findByKey('Business Name').field('Value');
-const volUnits = libByName(`${bizName} Settings`).findByKey('Volume Units').field('Value');
+const bizName = () => libByName('Boozehound Settings').findByKey('Business Name').field('Value');
+const volUnits = () => libByName(`${bizName()} Settings`).findByKey('Volume Units').field('Value');
 /*  massUnits: () => libByName(`${this.bizName} Settings`).findByKey('Mass Units').field('Value'),
   salesTaxRate: () => libByName(`${this.bizName} Settings`).findByKey('Sales Tax Rate').field('Value'),
   decPlaceAcc: () => libByName(`${this.bizName} Settings`).findByKey('Decimal Place Accuracy').field('Value'),
