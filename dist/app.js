21727b5de4d366c1db0052c06b1bf533ea75705e
Christian Burkhart <worm@christianburkhart.com>
2018-11-28 23:27:47 -0800

    test different syntax for export

--- dist/app.js
+++ dist/app.js
@@ -87,26 +87,15 @@ var app =
 /************************************************************************/
 /******/ ({
 
-/***/ "./node_modules/webpack/buildin/harmony-module.js":
-/*!*******************************************!*\
-  !*** (webpack)/buildin/harmony-module.js ***!
-  \*******************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-eval("module.exports = function(originalModule) {\n\tif (!originalModule.webpackPolyfill) {\n\t\tvar module = Object.create(originalModule);\n\t\t// module.parent = undefined by default\n\t\tif (!module.children) module.children = [];\n\t\tObject.defineProperty(module, \"loaded\", {\n\t\t\tenumerable: true,\n\t\t\tget: function() {\n\t\t\t\treturn module.l;\n\t\t\t}\n\t\t});\n\t\tObject.defineProperty(module, \"id\", {\n\t\t\tenumerable: true,\n\t\t\tget: function() {\n\t\t\t\treturn module.i;\n\t\t\t}\n\t\t});\n\t\tObject.defineProperty(module, \"exports\", {\n\t\t\tenumerable: true\n\t\t});\n\t\tmodule.webpackPolyfill = 1;\n\t}\n\treturn module;\n};\n\n\n//# sourceURL=webpack://app/(webpack)/buildin/harmony-module.js?");
-
-/***/ }),
-
 /***/ "./src/app.js":
 /*!********************!*\
   !*** ./src/app.js ***!
   \********************/
-/*! no exports provided */
+/*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var _usrPref__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./usrPref */ \"./src/usrPref.js\");\n\nmodule.exports = {\n  main: function main(bizName) {\n    //  let settings = new Settings();\n    return message(bizName);\n  }\n};\n/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ \"./node_modules/webpack/buildin/harmony-module.js\")(module)))\n\n//# sourceURL=webpack://app/./src/app.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return main; });\n/* harmony import */ var _usrPref__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./usrPref */ \"./src/usrPref.js\");\n\nfunction main(bizName) {\n  //  let settings = new Settings();\n  return message(bizName);\n}\n/*\nmodule.exports = {\n  main(bizName) {\n  //  let settings = new Settings();\n    return message(bizName);\n  },\n  settings: new Settings(),\n};\n*/\n\n//# sourceURL=webpack://app/./src/app.js?");
 
 /***/ }),
 
--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import Settings from './usrPref';
 
 
+export default function main(bizName) {
+//  let settings = new Settings();
+  return message(bizName);
+}
+/*
 module.exports = {
   main(bizName) {
   //  let settings = new Settings();
     return message(bizName);
   },
+  settings: new Settings(),
 };
+*/
