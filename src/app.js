865fc2d2acb29e797156f5c76627cfd7b399cf99
Christian Burkhart <worm@christianburkhart.com>
2018-12-01 14:21:42 -0800

    test

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+
+import UserSettings from './modules/UserSettings';
 import setFields from './modules/entry/setFields';
 
+// Global shorthand variable;
 var e = entry();
+
+// Main app function;
+function app(bizName, functionName) {
+  // Instantiate user settings;
+  const userSettings = new UserSettings(bizName);
+
+  return functionName;
+}
+
+// MementoDB script should execute here;
