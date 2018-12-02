f52f124bcbb0fe539e6aef1716bf1825a3af9a8d
Christian Burkhart <worm@christianburkhart.com>
2018-12-01 18:32:14 -0800

    test

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ export default function app(bizName, functionName) {
   // Instantiate user settings;
   const userSettings = new UserSettings(bizName);
 
-  return functionName;
+  return functionName();
 }
 
 // MementoDB script should execute here;
