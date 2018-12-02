1f79dfd2940f3f8e952b97c8703f81841e6d37d6
Christian Burkhart <worm@christianburkhart.com>
2018-12-01 21:55:17 -0800

    test

--- src/app.js
+++ src/app.js
@@ -4,15 +4,18 @@ import UserSettings from './modules/UserSettings';
 // Global shorthand variable;
 const e = entry();
 
-// Instantiate user settings;
-const userSettings = new UserSettings(setInstance);
-
 // Main app function;
 function setInstance(instance) {
   return instance;
 }
 
+function getSettings(instance) {
+  // Instantiate user settings;
+  const userSettings = new UserSettings(instance);
+}
+
 // Expose modules for use outside bundle;
 module.exports = {
   setInstance,
+  getSettings,
 }
