8214f0a769716e415d0bc670a1ba072eeedaa0b2
Christian Burkhart <worm@christianburkhart.com>
2018-12-02 22:59:53 -0800

    testing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import UserSettings from './modules/UserSettings';
 import setFields from './modules/entry/setFields';
 
-// Instantiate user settings;
-// const userSettings = new UserSettings(instance);
-
 function app(instance, desiredFunction) {
   return userSettings.instance;
 }
