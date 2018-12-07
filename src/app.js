7ed2c2b487c7326d805d9040ea892d05a04a5d2b
Christian Burkhart <worm@christianburkhart.com>
2018-12-06 23:34:50 -0800

    test

--- src/app.js
+++ src/app.js
@@ -1,17 +1,4 @@
-import { instanceLibs } from './modules/libraries';
 import getInstance from './modules/getInstance';
-import UserSettings from './modules/UserSettings';
-import setFields from './modules/entry/setFields';
-
-const instance = getInstance(instanceLibs);
-const userSettings = new UserSettings(instance);
-
-function app(instance, desiredFunction) {
-  return userSettings.instance;
-}
 
 // MementoDB script called here;
-export { UserSettings };
-export { setFields };
-export { app };
 export { getInstance };
