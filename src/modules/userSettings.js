8c55725ea105cdd99e21da849fdf0f0bb37f9add
Christian Burkhart <worm@christianburkhart.com>
2018-12-07 06:46:00 -0800

    test

--- src/modules/userSettings.js
+++ src/modules/userSettings.js
@@ -1,10 +1,10 @@
 import getInstance from './getInstance';
 
 const userSettings = {
-  volUnits: libByName(`${getInstance} Settings`).findByKey('Volume Units').field('Value'),
-  massUnits: libByName(`${getInstance} Settings`).findByKey('Mass Units').field('Value'),
-  salesTaxRate: libByName(`${getInstance} Settings`).findByKey('Sales Tax Rate').field('Value'),
-  decPlaceAcc: libByName(`${getInstance} Settings`).findByKey('Decimal Place Accuracy').field('Value'),
+  volUnits: libByName(`${getInstance()} Settings`).findByKey('Volume Units').field('Value'),
+  massUnits: libByName(`${getInstance()} Settings`).findByKey('Mass Units').field('Value'),
+  salesTaxRate: libByName(`${getInstance()} Settings`).findByKey('Sales Tax Rate').field('Value'),
+  decPlaceAcc: libByName(`${getInstance()} Settings`).findByKey('Decimal Place Accuracy').field('Value'),
 };
 
 export default userSettings;
